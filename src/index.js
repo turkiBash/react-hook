0a2e3f13087bf758fa7d013859f237041772e582
turkibash <dev@turki.tech>
2021-09-01 15:15:26 +0300

    Added muck server

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// const config = {
-//   initialColorMode: "light",
-//   useSystemColorMode: false,
-// };
+if (process.env.NODE_ENV === 'development') {
+
+  const { worker } = require('./mocks/browser')
+
+  worker.start()
+
+}
 
 ReactDOM.render(
   <React.StrictMode>
